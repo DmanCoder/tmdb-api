96d6fc1ed2939483cb85a647d09160b9ffcdb06c
DmanCoder <ktdenis.1@live.com>
2021-05-16 16:01:37 +1000

    setup free route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 // Define routes
 app.use('/api/popular/tv', require('./routes/api/popular/tv'));
+app.use('/api/tv/on_the_air', require('./routes/api/free/tv'));
 
 const PORT = process.env.PORT || 5000;
 
